37faa9b83bb43eb6623a3629a31fe9954496070e
Chandra Sekhar Kumar Pasagadugula <ienthusiastic.chandra@outlook.com>
2023-05-14 16:08:17 +0530

    breadcrumb changes

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss']
 })
-export class AppHeaderComponent {
-  activeId = '1';
+export class AppHeaderComponent implements OnInit {
+
+  activeId: Number | undefined = undefined;
+
+  ngOnInit() {
+    this.activeId = 1;
+  }
 }
